27f8fef34b5f871e3d21d70adedafd5ce0d35604
CodebySawan <work.sawanpandey@gmail.com>
2025-10-19 23:41:23 +0530

    Fix CORS - allow all origins

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ require('dotenv').config();
 const app = express();
 
 // Middleware - Simple CORS that works everywhere
+// Middleware - Simple CORS that works everywhere
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
